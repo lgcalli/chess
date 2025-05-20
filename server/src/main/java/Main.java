69776b46877642eb2aa5acb0545dd579a0d2f1be
lgcalli <lgcallister@gmail.com>
2025-05-19 18:08:39 -0600

    Phase 3 - Added model records

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -5,4 +5,6 @@ public class Main {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Server: " + piece);
     }
+
+
 }
