383085cb5939b2fd23b29116f91aa2b5a50a7172
lgcalli <122925928+lgcalli@users.noreply.github.com>
2025-06-04 17:13:59 -0600

    Phase 5 - starting on client ui

--- client/src/main/java/ui/ChessClient.java
+++ client/src/main/java/ui/ChessClient.java
@@ -1,5 +1,12 @@
 package ui;
+import server.ServerFacade;
 
 public class ChessClient {
+    private final ServerFacade server;
+    private final String serverUrl;
 
+    public ChessClient(String serverUrl) {
+        server = new ServerFacade(serverUrl);
+        this.serverUrl = serverUrl;
+    }
 }
